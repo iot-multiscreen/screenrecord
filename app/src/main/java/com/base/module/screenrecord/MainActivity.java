b3a45b63aedd54115b8442e857a1e8b74d54cada
luoye <841522821@qq.com>
2021-12-08 23:01:49 +0800

    add the screen record demo, it need android.uid.system Permission ,because the virtualdisplay

---
+++ app/src/main/java/com/base/module/screenrecord/MainActivity.java
@@ -0,0 +1,76 @@
+package com.base.module.screenrecord;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.util.Log;
+import android.view.View;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.base.module.screenrecord.databinding.ActivityMainBinding;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+    private final static String TAG = "SCREENRECORD.MainActivity";
+    private Button mStarScreenRecord;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private void startRecordService(){
+        Intent mIntent = new Intent();
+        mIntent.setClass(MainActivity.this,RecordService.class);
+        startService(mIntent);
+        finish();
+    }
+
+    private void initLayout(){
+        mStarScreenRecord = (Button) findViewById(R.id.start_record);
+        mStarScreenRecord.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startRecordService();
+            }
+        });
+    }
+
+    private void checkPermission(){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+        }
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        initLayout();
+        checkPermission();
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+    }
+}
---
+++ app/src/main/java/com/base/module/screenrecord/RecordService.java
@@ -0,0 +1,175 @@
+package com.base.module.screenrecord;
+
+import android.app.Service;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.graphics.Point;
+import android.hardware.display.DisplayManager;
+import android.media.MediaCodec;
+import android.media.MediaCodecInfo;
+import android.media.MediaFormat;
+import android.os.IBinder;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.Display;
+import android.view.Surface;
+import android.view.WindowManager;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+public class RecordService extends Service {
+    private final static String TAG = "ScreenRecord.RecordService";
+    private MediaCodec mMediaCodec;
+    private MediaFormat mMediaFormat;
+    private int mScreen_width,mScreen_height,mScreen_orientation,mScreen_DPI;
+    private DisplayManager mDisplayManager;
+    private Surface mSurface;
+    private EncordThread mEncordThread;
+    private final static String VIRTUALDEVICE = "screenrecord";
+    public RecordService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d(TAG,"onCreate");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(TAG,"onStartCommand");
+        try {
+            createEncoderMediacodec();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        createVirtualDisplay();
+        mEncordThread = new EncordThread();
+        mEncordThread.run();
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public ComponentName startService(Intent service) {
+        Log.d(TAG,"startService");
+        return super.startService(service);
+    }
+
+    /*create the encoder mediacodec
+       mediacodec createInputSurface usefor Binding virtualdisplay
+    * */
+    private void createEncoderMediacodec() throws IOException {
+        Log.d(TAG,"createEncoder");
+        getScreenRes();
+        initMediaFormat();
+        mSurface = mMediaCodec.createInputSurface();
+        mMediaCodec.start();
+    }
+
+    //get current screen size
+    private void getScreenRes(){
+        Log.d(TAG,"getScreenRes");
+        Display mDisplay = ((WindowManager)getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
+
+        Point size = new Point();
+        mDisplay.getRealSize(size);
+        Configuration config = getResources().getConfiguration();
+        mScreen_width = size.x;
+        mScreen_height = size.y;
+        mScreen_orientation = config.orientation;
+        mScreen_DPI = Math.min(mScreen_width,mScreen_height)* DisplayMetrics.DENSITY_XHIGH / 1080;
+    }
+
+    // init the media encoder media format and configure the mediacodec
+    private void initMediaFormat() throws IOException {
+        Log.d(TAG,"initMediaFormat mScreen_width="+mScreen_width+" mScreen_height="+mScreen_height);
+        mMediaFormat = MediaFormat.createVideoFormat(MediaFormat.MIMETYPE_VIDEO_AVC,mScreen_width,mScreen_height);
+        mMediaFormat.setInteger(MediaFormat.KEY_COLOR_FORMAT,MediaCodecInfo.CodecCapabilities.COLOR_FormatSurface);
+        mMediaFormat.setInteger(MediaFormat.KEY_FRAME_RATE,60);
+        mMediaFormat.setInteger(MediaFormat.KEY_BIT_RATE,4*1024*1024);
+        mMediaFormat.setInteger(MediaFormat.KEY_I_FRAME_INTERVAL,1);
+        mMediaFormat.setInteger(MediaFormat.KEY_PROFILE, MediaCodecInfo.CodecProfileLevel.AVCProfileBaseline);
+        mMediaFormat.setInteger(MediaFormat.KEY_LEVEL,MediaCodecInfo.CodecProfileLevel.AVCLevel3);
+        mMediaFormat.setInteger(MediaFormat.KEY_WIDTH,mScreen_width);
+        mMediaFormat.setInteger(MediaFormat.KEY_HEIGHT,mScreen_height);
+        mMediaFormat.setInteger(MediaFormat.KEY_REPEAT_PREVIOUS_FRAME_AFTER,5000);
+        mMediaFormat.setInteger(MediaFormat.KEY_PREPEND_HEADER_TO_SYNC_FRAMES,1);
+        mMediaCodec = MediaCodec.createEncoderByType("video/avc");
+        mMediaCodec.configure(mMediaFormat,null,null, MediaCodec.CONFIGURE_FLAG_ENCODE);
+    }
+
+    // create the virtualdisplay neet system.uid permission
+    private void createVirtualDisplay(){
+        if (null == mDisplayManager){
+            mDisplayManager = (DisplayManager)getSystemService(Context.DISPLAY_SERVICE);
+        }
+        int flags = 0;
+        flags |= DisplayManager.VIRTUAL_DISPLAY_FLAG_PUBLIC
+                | DisplayManager.VIRTUAL_DISPLAY_FLAG_PRESENTATION;
+        mDisplayManager.createVirtualDisplay(VIRTUALDEVICE, mScreen_width, mScreen_height, mScreen_DPI, mSurface, flags);
+    }
+
+    //not used ,pre for save the encode data
+    private void saveEncorderData(byte[] bfile, String filePath,String fileName) {
+        BufferedOutputStream bos = null;
+        FileOutputStream fos = null;
+        File file = null;
+        try {
+            File dir = new File(filePath);
+            if(!dir.exists()&&dir.isDirectory()){//check the directory is exist
+                dir.mkdirs();
+            }
+            file = new File(filePath + fileName);
+            fos = new FileOutputStream(file);
+            bos = new BufferedOutputStream(fos);
+            bos.write(bfile);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (bos != null) {
+                try {
+                    bos.close();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }
+            if (fos != null) {
+                try {
+                    fos.close();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        }
+    }
+
+    //add the thread for encord
+    private class EncordThread extends Thread{
+        @Override
+        public void run() {
+            super.run();
+            MediaCodec.BufferInfo bufferInfo = new MediaCodec.BufferInfo();
+            ByteBuffer byteBuffer ;
+            while (true){
+                int outindex = mMediaCodec.dequeueOutputBuffer(bufferInfo,2*1000);
+                if(outindex >= 0){
+                    byteBuffer = mMediaCodec.getOutputBuffer(outindex);
+                    mMediaCodec.releaseOutputBuffer(outindex,false);
+                    Log.d(TAG,"outindex="+outindex);
+                }
+            }
+        }
+    }
+}
